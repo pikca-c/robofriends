b8fa8127c38242dddf70be58cdeb01be49e32e83
pikca-c <vincimchan@gmail.com>
2024-01-12 21:32:42 +0000

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Cardlist from "./CardList";
+import { robots } from "./robots";
+
+const App = () => {
+  <div>
+    <h1>RoboFriends</h1>
+    <Cardlist robots={robots} />
+  </div>;
+};
+
+export default App;
--- src/CardList.js
+++ src/CardList.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import Card from './Card';
-
+import React from "react";
+import Card from "./Card";
 
 const CardList = ({ robots }) => {
-    const cardComponent = robots.map((user, i) => {
-        return <Card key={i}
-        id={robots[i].id}
-        name={robots[i].name}
-        email={robots[i].email}/>
-    })
-    return (
-        <div>
-            {cardComponent}
-        </div>
-    );
-}
+  return (
+    <div>
+      {robots.map((user, i) => {
+        return (
+          <Card key={i} id={user.id} name={user.name} email={user.email} />
+        );
+      })}
+    </div>
+  );
+};
 
 export default CardList;
