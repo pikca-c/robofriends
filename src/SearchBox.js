7f05cf156bbc6a3b529438f57d3f22a5664132cb
pikca-c <vincimchan@gmail.com>
2024-01-14 15:11:15 +0000

    update

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
-class SearchBox extends React.Component {
-  render() {
-    return (
-      <div id="searchbox" className="pa2">
-        <input
-          className="pa2 ba b--green bg-lightest-blue"
-          type="search"
-          placeholder="search robots"
-        />
+const SearchBox = () => {
+  const [seacrchText, setSearchText] = useState();
+  return (
+    <div id="searchbox" className="pa2">
+      <input
+        className="pa2 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robots"
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <div>
+        <p>{seacrchText}</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default SearchBox;
--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ root.render(
   </div>
 );
 
+//idk maybe lets try whatsapp
 // ReactDOM.render(<App />, document.getElementById("root"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
