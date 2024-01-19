2657b9d8b678ae602ba5f6ef933e5b21cfb43592
pikca-c <vincimchan@gmail.com>
2024-01-19 22:34:18 +0000

    added didmount

--- src/App.js
+++ src/App.js
@@ -8,9 +8,13 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+
+  componentDidMount() {
+    this.setState({ robots: robots });
   }
 
   onSearchChange = (event) => {
