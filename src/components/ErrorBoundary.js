f249df85249a658d639c9ae990595117a47eb452
pikca-c <vincimchan@gmail.com>
2024-01-24 22:51:30 +0000

    Added error boundary

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,4 +1,18 @@
-Updated;
+import React, { Component } from "react";
 
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+  render() {
+    if (this.state.hasError) {
+      return <h1>Oops, something went wrong</h1>;
+    }
+    return this.props.children;
+  }
+}
 
-
+export default ErrorBoundary;
